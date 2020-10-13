3f74da14fbdba7284d476c72581016fd02298c3a
Lourmati <49345524+Lourmati@users.noreply.github.com>
2020-10-12 23:47:31 -0400

    Add files

---
+++ Labo3/Form1.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace Labo3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TxtBox1 = new System.Windows.Forms.TextBox();
+            this.TxtBox2 = new System.Windows.Forms.TextBox();
+            this.Lbl1 = new System.Windows.Forms.Label();
+            this.Lbl2 = new System.Windows.Forms.Label();
+            this.Lbl3 = new System.Windows.Forms.Label();
+            this.Lbl4 = new System.Windows.Forms.Label();
+            this.Lbl5 = new System.Windows.Forms.Label();
+            this.RadioBtn1 = new System.Windows.Forms.RadioButton();
+            this.ComboBox1 = new System.Windows.Forms.ComboBox();
+            this.RadioBtn2 = new System.Windows.Forms.RadioButton();
+            this.CheckedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.Lbl6 = new System.Windows.Forms.Label();
+            this.TxtBox3 = new System.Windows.Forms.TextBox();
+            this.Btn1 = new System.Windows.Forms.Button();
+            this.ErrorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.Pnl1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.CheckBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ErrorProvider1)).BeginInit();
+            this.Pnl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TxtBox1
+            // 
+            this.TxtBox1.Location = new System.Drawing.Point(233, 139);
+            this.TxtBox1.Name = "TxtBox1";
+            this.TxtBox1.Size = new System.Drawing.Size(339, 20);
+            this.TxtBox1.TabIndex = 0;
+            // 
+            // TxtBox2
+            // 
+            this.TxtBox2.Location = new System.Drawing.Point(233, 187);
+            this.TxtBox2.Name = "TxtBox2";
+            this.TxtBox2.Size = new System.Drawing.Size(339, 20);
+            this.TxtBox2.TabIndex = 1;
+            // 
+            // Lbl1
+            // 
+            this.Lbl1.AutoSize = true;
+            this.Lbl1.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.Lbl1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Lbl1.Location = new System.Drawing.Point(25, 141);
+            this.Lbl1.Name = "Lbl1";
+            this.Lbl1.Size = new System.Drawing.Size(142, 18);
+            this.Lbl1.TabIndex = 2;
+            this.Lbl1.Text = "Nom du Pokémon : ";
+            // 
+            // Lbl2
+            // 
+            this.Lbl2.AutoSize = true;
+            this.Lbl2.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.Lbl2.Location = new System.Drawing.Point(25, 187);
+            this.Lbl2.Name = "Lbl2";
+            this.Lbl2.Size = new System.Drawing.Size(159, 18);
+            this.Lbl2.TabIndex = 3;
+            this.Lbl2.Text = "Numéro du Pokémon :";
+            // 
+            // Lbl3
+            // 
+            this.Lbl3.AutoSize = true;
+            this.Lbl3.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.Lbl3.Location = new System.Drawing.Point(26, 257);
+            this.Lbl3.Name = "Lbl3";
+            this.Lbl3.Size = new System.Drawing.Size(61, 18);
+            this.Lbl3.TabIndex = 5;
+            this.Lbl3.Text = "Genre : ";
+            // 
+            // Lbl4
+            // 
+            this.Lbl4.AutoSize = true;
+            this.Lbl4.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.Lbl4.Location = new System.Drawing.Point(25, 300);
+            this.Lbl4.Name = "Lbl4";
+            this.Lbl4.Size = new System.Drawing.Size(101, 18);
+            this.Lbl4.TabIndex = 6;
+            this.Lbl4.Text = "Générations : ";
+            // 
+            // Lbl5
+            // 
+            this.Lbl5.AutoSize = true;
+            this.Lbl5.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.Lbl5.Location = new System.Drawing.Point(26, 345);
+            this.Lbl5.Name = "Lbl5";
+            this.Lbl5.Size = new System.Drawing.Size(52, 18);
+            this.Lbl5.TabIndex = 8;
+            this.Lbl5.Text = "Type : ";
+            // 
+            // RadioBtn1
+            // 
+            this.RadioBtn1.AutoSize = true;
+            this.RadioBtn1.BackColor = System.Drawing.Color.Transparent;
+            this.RadioBtn1.Location = new System.Drawing.Point(3, 24);
+            this.RadioBtn1.Name = "RadioBtn1";
+            this.RadioBtn1.Size = new System.Drawing.Size(48, 17);
+            this.RadioBtn1.TabIndex = 9;
+            this.RadioBtn1.TabStop = true;
+            this.RadioBtn1.Text = "Mâle";
+            this.RadioBtn1.UseVisualStyleBackColor = false;
+            // 
+            // ComboBox1
+            // 
+            this.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboBox1.FormattingEnabled = true;
+            this.ComboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7"});
+            this.ComboBox1.Location = new System.Drawing.Point(233, 301);
+            this.ComboBox1.Name = "ComboBox1";
+            this.ComboBox1.Size = new System.Drawing.Size(339, 21);
+            this.ComboBox1.TabIndex = 7;
+            // 
+            // RadioBtn2
+            // 
+            this.RadioBtn2.AutoSize = true;
+            this.RadioBtn2.BackColor = System.Drawing.Color.Transparent;
+            this.RadioBtn2.Location = new System.Drawing.Point(132, 24);
+            this.RadioBtn2.Name = "RadioBtn2";
+            this.RadioBtn2.Size = new System.Drawing.Size(61, 17);
+            this.RadioBtn2.TabIndex = 10;
+            this.RadioBtn2.TabStop = true;
+            this.RadioBtn2.Text = "Femelle";
+            this.RadioBtn2.UseVisualStyleBackColor = false;
+            // 
+            // CheckedListBox1
+            // 
+            this.CheckedListBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(181)))), ((int)(((byte)(23)))));
+            this.CheckedListBox1.CheckOnClick = true;
+            this.CheckedListBox1.FormattingEnabled = true;
+            this.CheckedListBox1.Items.AddRange(new object[] {
+            "Normal",
+            "Eau",
+            "Feu",
+            "Plante",
+            "Électrik",
+            "Glace",
+            "Combat",
+            "Poison",
+            "Sol",
+            "Vol",
+            "Psy",
+            "Insecte",
+            "Roche",
+            "Dragon",
+            "Fée",
+            "Acier"});
+            this.CheckedListBox1.Location = new System.Drawing.Point(234, 345);
+            this.CheckedListBox1.MultiColumn = true;
+            this.CheckedListBox1.Name = "CheckedListBox1";
+            this.CheckedListBox1.Size = new System.Drawing.Size(339, 79);
+            this.CheckedListBox1.TabIndex = 11;
+            this.CheckedListBox1.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.CheckedListBox1_ItemCheck);
+            // 
+            // Lbl6
+            // 
+            this.Lbl6.AutoSize = true;
+            this.Lbl6.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl6.Enabled = false;
+            this.Lbl6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.Lbl6.Location = new System.Drawing.Point(231, 452);
+            this.Lbl6.Name = "Lbl6";
+            this.Lbl6.Size = new System.Drawing.Size(147, 18);
+            this.Lbl6.TabIndex = 13;
+            this.Lbl6.Text = "Nom du propriétaire :";
+            // 
+            // TxtBox3
+            // 
+            this.TxtBox3.Enabled = false;
+            this.TxtBox3.Location = new System.Drawing.Point(384, 450);
+            this.TxtBox3.Name = "TxtBox3";
+            this.TxtBox3.Size = new System.Drawing.Size(188, 20);
+            this.TxtBox3.TabIndex = 14;
+            // 
+            // Btn1
+            // 
+            this.Btn1.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.Btn1.FlatAppearance.BorderColor = System.Drawing.Color.Yellow;
+            this.Btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.Btn1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Btn1.Location = new System.Drawing.Point(474, 499);
+            this.Btn1.Name = "Btn1";
+            this.Btn1.Size = new System.Drawing.Size(99, 71);
+            this.Btn1.TabIndex = 15;
+            this.Btn1.Text = "Enregistrer";
+            this.Btn1.UseVisualStyleBackColor = false;
+            this.Btn1.Click += new System.EventHandler(this.Btn1_Click);
+            // 
+            // ErrorProvider1
+            // 
+            this.ErrorProvider1.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
+            this.ErrorProvider1.ContainerControl = this;
+            // 
+            // Pnl1
+            // 
+            this.Pnl1.BackColor = System.Drawing.Color.Transparent;
+            this.Pnl1.Controls.Add(this.RadioBtn1);
+            this.Pnl1.Controls.Add(this.RadioBtn2);
+            this.Pnl1.Location = new System.Drawing.Point(234, 234);
+            this.Pnl1.Name = "Pnl1";
+            this.Pnl1.Size = new System.Drawing.Size(202, 44);
+            this.Pnl1.TabIndex = 16;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Labo3.Properties.Resources.pokemon_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(233, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(141, 91);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Labo3.Properties.Resources.pokeball;
+            this.pictureBox2.Location = new System.Drawing.Point(164, 24);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(63, 64);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 18;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Labo3.Properties.Resources.pokeball;
+            this.pictureBox3.Location = new System.Drawing.Point(380, 24);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(63, 64);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 19;
+            this.pictureBox3.TabStop = false;
+            // 
+            // CheckBox1
+            // 
+            this.CheckBox1.AutoSize = true;
+            this.CheckBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.CheckBox1.Location = new System.Drawing.Point(28, 455);
+            this.CheckBox1.Name = "CheckBox1";
+            this.CheckBox1.Size = new System.Drawing.Size(73, 22);
+            this.CheckBox1.TabIndex = 20;
+            this.CheckBox1.Text = "Attrapé";
+            this.CheckBox1.UseVisualStyleBackColor = true;
+            this.CheckBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(190)))), ((int)(((byte)(11)))));
+            this.BackgroundImage = global::Labo3.Properties.Resources.pokemon;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(600, 595);
+            this.Controls.Add(this.CheckBox1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Pnl1);
+            this.Controls.Add(this.Btn1);
+            this.Controls.Add(this.TxtBox3);
+            this.Controls.Add(this.Lbl6);
+            this.Controls.Add(this.CheckedListBox1);
+            this.Controls.Add(this.Lbl5);
+            this.Controls.Add(this.ComboBox1);
+            this.Controls.Add(this.Lbl4);
+            this.Controls.Add(this.Lbl3);
+            this.Controls.Add(this.Lbl2);
+            this.Controls.Add(this.Lbl1);
+            this.Controls.Add(this.TxtBox2);
+            this.Controls.Add(this.TxtBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Fiche Pokémon";
+            ((System.ComponentModel.ISupportInitialize)(this.ErrorProvider1)).EndInit();
+            this.Pnl1.ResumeLayout(false);
+            this.Pnl1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtBox1;
+        private System.Windows.Forms.TextBox TxtBox2;
+        private System.Windows.Forms.Label Lbl1;
+        private System.Windows.Forms.Label Lbl2;
+        private System.Windows.Forms.Label Lbl3;
+        private System.Windows.Forms.Label Lbl4;
+        private System.Windows.Forms.Label Lbl5;
+        private System.Windows.Forms.RadioButton RadioBtn1;
+        private System.Windows.Forms.ComboBox ComboBox1;
+        private System.Windows.Forms.RadioButton RadioBtn2;
+        private System.Windows.Forms.CheckedListBox CheckedListBox1;
+        private System.Windows.Forms.Label Lbl6;
+        private System.Windows.Forms.TextBox TxtBox3;
+        private System.Windows.Forms.Button Btn1;
+        private System.Windows.Forms.ErrorProvider ErrorProvider1;
+        private System.Windows.Forms.Panel Pnl1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.CheckBox CheckBox1;
+    }
+}
+
---
+++ Labo3/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labo3
+{
+    /// <summary>
+    /// Form1, contenant tout les composantes de ma fenetre
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Initialisation des composantes
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+            MaximizeBox = false;
+        }
+
+        /// <summary>
+        /// Méthode du bouton enregistrer, validation de chaque composante avant de rénitialiser
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Btn1_Click(object sender, EventArgs e)
+        {
+            bool estValide = true;
+            int compteur = 0;
+
+            estValide = Validation_TxtBox1();
+            if (estValide)
+            {
+                compteur++;
+            }
+
+            estValide = Validation_TxtBox2();
+            if (estValide)
+            {
+                compteur++;
+            }
+
+            estValide = Validation_TxtBox3();
+            if (estValide)
+            {
+                compteur++;
+            }
+
+            estValide = Validation_ComboBox1();
+            if (estValide)
+            {
+                compteur++;
+            }
+
+            estValide = Validation_CheckedListBox1();
+            if (estValide)
+            {
+                compteur++;
+            }
+
+            estValide = Validation_Pnl1();
+            if (estValide)
+            {
+                compteur++;
+            }
+
+            if (compteur == 6)
+            {
+                //Renitialiser
+                Renitialisation();
+            }
+        }
+
+        /// <summary>
+        /// Méthode pour la validation du TxtBox1
+        /// </summary>
+        /// <returns></returns>
+        private bool Validation_TxtBox1()
+        {
+            ErrorProvider1.SetError(TxtBox1, "");
+            bool estValide = true;
+
+            TxtBox1.Text = TxtBox1.Text.Trim();
+            if (String.IsNullOrEmpty(TxtBox1.Text))
+            {
+                estValide = false;
+                ErrorProvider1.SetError(TxtBox1, "Le champ ne peut être vide");
+            }
+
+
+            foreach (char c in TxtBox1.Text)
+            {
+                if (!char.IsLetter(c))
+                {
+                    estValide = false;
+                    ErrorProvider1.SetError(TxtBox1, "Le nom est composé seuleument de lettres.");
+                }
+            }
+            return estValide;
+        }
+
+
+        /// <summary>
+        /// Méthode pour la validation du TxtBox2
+        /// </summary>
+        /// <returns></returns>
+        private bool Validation_TxtBox2()
+        {
+            ErrorProvider1.SetError(TxtBox2, "");
+            bool estValide = true;
+            TxtBox2.Text = TxtBox2.Text.Trim();
+
+            if (String.IsNullOrEmpty(TxtBox2.Text))
+            {
+                estValide = false;
+                ErrorProvider1.SetError(TxtBox2, "Le champ ne peut être vide");
+            }
+
+            foreach (char c in TxtBox2.Text)
+            {
+                if (!Char.IsDigit(c))
+                {
+                    estValide = false;
+                    ErrorProvider1.SetError(TxtBox2, "Le numéro est composé de chiffres.");
+                }
+            }
+
+            if (estValide)
+            {
+                if (int.Parse(TxtBox2.Text) > 809)
+                {
+                    estValide = false;
+                    ErrorProvider1.SetError(TxtBox2, "Le maximum est de 809");
+                }
+            }
+
+            return estValide;
+        }
+
+
+        /// <summary>
+        /// Méthode pour la validation du TxtBox3
+        /// </summary>
+        /// <returns></returns>
+        private bool Validation_TxtBox3()
+        {
+            ErrorProvider1.SetError(TxtBox3, "");
+            bool estValide = true;
+            TxtBox3.Text = TxtBox3.Text.Trim();
+
+            if (TxtBox3.Enabled)/// Si le pokémon est attrapé
+            {
+                if (String.IsNullOrEmpty(TxtBox3.Text))
+                {
+                    estValide = false;
+                    ErrorProvider1.SetError(TxtBox3, "Le champ ne peut être vide");
+                }
+
+                //Code défectueux
+                //foreach (char c in TxtBox3.Text)
+                //{
+                //    if (!Char.IsLetter(c))
+                //    {
+                //        estValide = false;
+                //        ErrorProvider1.SetError(TxtBox3, "Le nom du propriétaire est composé seuleument de lettres.");
+                //    }
+                //}
+            }
+
+            return estValide;
+        }
+
+
+        /// <summary>
+        /// Méthode pour la validation du ComboBox1
+        /// </summary>
+        /// <returns></returns>
+        private bool Validation_ComboBox1()
+        {
+            ErrorProvider1.SetError(ComboBox1, "");
+            bool estValide = true;
+            if (ComboBox1.SelectedIndex == -1)
+            {
+                estValide = false;
+                ErrorProvider1.SetError(ComboBox1, "Veuillez sélectionner un élement");
+            }
+
+            return estValide;
+        }
+
+
+        /// <summary>
+        /// Méthode pour la validation du CheckedListBox (élements)
+        /// </summary>
+        /// <returns></returns>
+        private bool Validation_CheckedListBox1()
+        {
+            ErrorProvider1.SetError(CheckedListBox1, "");
+            bool estValide = true;
+
+            if (CheckedListBox1.CheckedItems.Count > 2)
+            {
+                estValide = false;
+                ErrorProvider1.SetError(CheckedListBox1, "Le maximum est de 2");
+            }
+
+            if (CheckedListBox1.CheckedItems.Count < 1)
+            {
+                estValide = false;
+                ErrorProvider1.SetError(CheckedListBox1, "Veuillez sélectionner au minimum 1 élement");
+            }
+
+            return estValide;
+        }
+
+
+        /// <summary>
+        /// Méthode pour la validation du Pnl1, contenant les radio buttons
+        /// </summary>
+        /// <returns></returns>
+        private bool Validation_Pnl1()
+        {
+            ErrorProvider1.SetError(Pnl1, "");
+            bool estValide = true;
+
+            if (!RadioBtn1.Checked && !RadioBtn2.Checked)
+            {
+                estValide = false;
+                ErrorProvider1.SetError(Pnl1, "Veuillez sélectionner un genre");
+            }
+
+            return estValide;
+        }
+
+
+        /// <summary>
+        /// Méthode de rénitialisation de la fenetre
+        /// </summary>
+        private void Renitialisation()
+        {
+            MessageBox.Show("Le pokémon " + TxtBox1.Text + " est enregistré avec succès. \n", "Enregistrement");
+
+            //Rénitialiser le tout
+            TxtBox1.Clear();
+            TxtBox2.Clear();
+            TxtBox3.Clear();
+            ComboBox1.SelectedItem = null;
+            CheckedListBox1.SelectedItem = null;
+            RadioBtn1.Checked = false;
+            RadioBtn2.Checked = false;
+            CheckBox1.Checked = false;
+            Lbl6.Enabled = false;
+            TxtBox3.Enabled = false;
+
+            foreach (int i in CheckedListBox1.CheckedIndices)
+            {
+                CheckedListBox1.SetItemCheckState(i, CheckState.Unchecked);
+            }
+        }
+
+
+        /// <summary>
+        /// Méthode pour l'activation du champ Nom du propriétaire si le pokémon est attrapé.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (CheckBox1.Checked)
+            {
+                Lbl6.Enabled = true;
+                TxtBox3.Enabled = true;
+            }
+            else
+            {
+                Lbl6.Enabled = false;
+                TxtBox3.Enabled = false;
+            }
+        }
+
+
+        /// <summary>
+        /// Méthode pour ajuster le CheckedBoxList (2 sélection maximum)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CheckedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            if (e.NewValue == CheckState.Checked && CheckedListBox1.CheckedItems.Count >= 2)
+                e.NewValue = CheckState.Unchecked;
+        }
+    }
+}
